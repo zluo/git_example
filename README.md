2664bed16aa4d88091a9d690823f997f160a81db
zluo <zhanqing.luo@vasco.com>
2017-01-18 16:31:00 -0500

    rebuild structure

---
+++ README.md
@@ -0,0 +1,8 @@
+h1. Git Example
+
+ A project to help learn git
+ 
+h2. How to use
+
+  Each folder represent a git subject, run build.sh to create a practice git repo in tmp folder,
+  then practice on that folder.  
--- rebase/create_ex.sh
+++ rebase/create_ex.sh
@@ -1,8 +1,5 @@
-
-cd /tmp
-rm gp -rf
-mkdir gp
-cd gp
+mkdir tmp
+cd tmp
 git init
 echo one > one.txt
 git add .
@@ -10,8 +7,8 @@ git commit -m "c1"
 echo two >> one.txt
 git add .
 git commit -m "c2"
-git branch experiment
-git checkout expriment
+git branch b1
+git checkout b1
 echo one > two.txt
 echo two >>two.txt
 git add .
